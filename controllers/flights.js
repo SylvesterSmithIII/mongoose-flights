27f28c2b9855db1de86970bd9097f3b4592bc452
SylvesterSmithIII <sylvestermsmithiii@gmail.com>
2023-07-14 13:20:06 -0700

    add sorting aing of flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,35 +2,43 @@ const Flight = require('../models/flight')
 
 module.exports = {
     new: newFlight,
-    index
+    index,
+    create
 }
 
 function newFlight(req, res) {
-    res.render('flights/new', {errorMsg: ''})
+    const newFlight = new Flight();
+    // Obtain the default date
+    const dt = newFlight.departs;
+    // Format the date for the value attribute of the input
+    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+    res.render('flights/new', { departsDate });
 }
 
-// async function create(req, res) {
-//     // convert nowShowing's checkbox of nothing or "on" to boolean
-//     req.body.nowShowing = !!req.body.nowShowing
-//     // remove any whitespace at start and end of cast
-//     req.body.cast = req.body.cast.trim()
-//     // split cast into an arra of ot an empty stirng
-//     if (req.body.cast) req.body.cast.split(/\s*,\s*/)
-
-//     for (let key in req.body) {
-//         if (req.body[key] === '') delete req.body[key]
-//     }
-    
-//     try {
-//         await Movie.create(req.body)
-//         res.redirect('/movies')
-//     } catch (err) {
-//         res.render('movies/new', {errorMsg: err.message})
-//     }
-// }
+async function create(req, res) {
+    try {
+        await Flight.create(req.body)
+        res.redirect('/flights')
+    } catch (err) {
+        res.render('flights/new', {errorMsg: err.message})
+    }
+}
 
 async function index(req, res) {
     const flights = await Flight.find ({})
+
+    flights.sort((a, b) => a.departs - b.departs)
+    
+    const currentDate = new Date()
+
+    flights.forEach(flight => {
+        if (new Date(flight.departs) < currentDate) {
+            flight.valid = false
+        } else {
+            flight.valid = true
+        }
+    })
     console.log(flights)
     res.render('flights/index', { flights })
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,10 +4,10 @@ var router = express.Router();
 const flightsCtrl = require('../controllers/flights')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', flightsCtrl.index)
 
 router.get('/new', flightsCtrl.new)
+
+router.post('/', flightsCtrl.create)
 
 module.exports = router;
